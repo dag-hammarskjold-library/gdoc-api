5400b77926aa4365403bc16d361ddeeaf8dadd53
jbukhari <joe.bukhari@gmail.com>
2021-04-16 15:23:25 -0400

    Update gdoc_dlx.py

--- gdoc_api/scripts/gdoc_dlx.py
+++ gdoc_api/scripts/gdoc_dlx.py
@@ -40,7 +40,10 @@ def set_log():
 ###
 
 def run(*, station=None, date=None, language=None, overwrite=None, **kwargs):
-    if station: sys.argv.append(f'--station={station}')
+    
+if station: sys.argv.append(f'--station={station}')
+    sys.argv = [sys.argv[0]]
+
     if date: sys.argv.append(f'--date={date}')
     if language: sys.argv.append(f'--language={language}')
     if overwrite: sys.argv.append('--overwrite')
